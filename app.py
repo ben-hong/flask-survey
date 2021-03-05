a51489bf6fe4624c7f6566d31663da866617376e
Ben Hong <benhong654@gmail.com>
2021-03-04 16:44:57 -0800

    made it so you can choose survey

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import surveys
 from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
@@ -10,10 +11,19 @@ debug = DebugToolbarExtension(app)
 
 @app.route("/")
 def index():
-    """Shows title, instructions, and button to route to survey questions"""
-    session["responses"] = []
+    """prompts which survey user wants"""
 
-    return render_template("survey_start.html", title=survey.title, instructions=survey.instructions)
+    return render_template("survey_choice.html", survey_type=surveys)
+
+@app.route("/start", methods=["POST"])
+def start():
+    """Shows title, instructions, and button to route to survey questions"""
+    survey_type = request.form['survey']
+    title = surveys[survey_type].title
+    instructions = surveys[survey_type].instructions
+    session['responses'] = []
+    session['survey'] = survey_type
+    return render_template("survey_start.html", title=title, instructions=instructions)
 
 @app.route("/begin", methods=["POST"])
 def begin():
@@ -38,7 +48,10 @@ def answer():
 @app.route("/questions/<int:qnum>")
 def questions(qnum):
     """populates page with survey questions"""
+    survey = surveys[session['survey']]
+    print(survey)
     num_answered = len(session['responses'])
+
     if (num_answered == len(survey.questions)):
         flash("Yo, you can't go there!")
         return redirect("/thankyou")
